81a4ffa6a68bdbaa773c21430fb1e14df819326f
manojpannala <manojpannala@outlook.com>
2021-05-03 14:31:53 +0300

    Update to Jenkinsfile

--- script.groovy
+++ script.groovy
@@ -7,13 +7,13 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh 'docker build -t manojpannala/java-maven-app:1.4 .'
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push manojpannala/java-maven-app:1.4'
     }
 }
 
 def deployApp() {
     echo 'deploying the application...'
+    sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+    sh 'docker push manojpannala/java-maven-app:1.4'
 }
 
 return this
