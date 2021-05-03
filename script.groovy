07849236cad83e94de725d5d258b3f840e284701
manojpannala <manojpannala@outlook.com>
2021-05-03 14:18:07 +0300

    Add script.groovy

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t manojpannala/java-maven-app .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push manojpannala/java-maven-app'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh 'docker build -t manojpannala/java-maven-app:1.4 .'
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh 'docker push manojpannala/java-maven-app:1.4'
     }
 }
 
