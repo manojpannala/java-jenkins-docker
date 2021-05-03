d0e57e27d78b3d13c30890fc4f042e2394b0bb73
manojpannala <manojpannala@outlook.com>
2021-05-03 16:59:49 +0300

    calling library from shared file

--- script.groovy
+++ script.groovy
@@ -1,19 +1,19 @@
 #!/usr/bin/env groovy
 
 
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-}
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t manojpannala/java-maven-app:2.1 .'
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push manojpannala/java-maven-app:2.1'
-    }
-}
+//def buildJar() {
+//    echo "building the application..."
+//    sh 'mvn package'
+//}
+//
+//def buildImage() {
+//    echo "building the docker image..."
+//    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+//        sh 'docker build -t manojpannala/java-maven-app:2.1 .'
+//        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+//        sh 'docker push manojpannala/java-maven-app:2.1'
+//    }
+//}
 
 def deployApp() {
     echo 'deploying the application...'
